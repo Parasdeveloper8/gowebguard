54caf70e72d37e6dec07a60a3390c876fe93e088
Paras prajapat <parasprajapat8th.gmail.com>
2025-01-19 16:46:47 +0530

    Added test folder

---
+++ test/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Parasdeveloper8/gowebguard/v2/webguard"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	/*headers := &webguard.Headers{
+		StrictTransportSecurity: "gegbhg",
+		XContentTypeOptions:     "hhjhu",
+		XFrameOptions:           "kjiji",
+		XXSSProtection:          "huhlhi",
+		ReferrerPolicy:          "bubuihi",
+	}*/
+	//headers := &webguard.Headers{ContentSecurityPolicy: "kop"}
+	r.Use(webguard.WebGuard())
+	header := &webguard.Csp{
+		ContentSecurityPolicy: "style-src:'self'",
+	}
+	r.Use(webguard.CSP(header))
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"Hello": "Check Headers"})
+	})
+	r.Run(":4000")
+}
